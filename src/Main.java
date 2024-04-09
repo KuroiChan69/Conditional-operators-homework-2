38a74413c40cad8946a65bcde8c6a1af5c89e936
KuroiChan69 <nikita.papshev@gmail.com>
2024-04-10 03:51:48 +0700

    Учёл поправки.

--- src/Main.java
+++ src/Main.java
@@ -29,8 +29,8 @@ public class Main {
                     break;
                 default:
                     System.out.println(" ваш аппарат не поддерживает актуальную версию приложения");
-            }}
-            else {
+            }
+        } else {
             switch (clientOS) {
                 case 0:
                     System.out.println("Ваш смартфон работает под управлением iOS");
@@ -49,7 +49,8 @@ public class Main {
         short currentYear = 2024;
         boolean cond1 = ((currentYear % 4) == 0);
         boolean cond2 = ((currentYear % 100) != 0);
-        boolean cond3 = currentYear < 1584;
+        boolean cond3 = currentYear <= 1584;
+        boolean cond4 = ((currentYear % 400) != 0);
         if (cond3) {
             System.out.println("В это время ещё не было понятия високосности");
         }
@@ -58,8 +59,10 @@ public class Main {
         else {
             if (cond1 && cond2) {
                 System.out.println("Указан " + currentYear + "й год, и он является високосным");
-            } else {
+            } else
                 System.out.println("указанный " + currentYear + "й год, и он не является високосным.");
+            if (cond4) {
+                System.out.println("Указан " + currentYear + "й год, и он является високосным");
             }
         }
         System.out.println(" ");
@@ -70,59 +73,73 @@ public class Main {
         boolean middle = (deliveryDistance >= 20) && (deliveryDistance < 60);
         boolean far = (deliveryDistance >= 60) && (deliveryDistance <= 100);
         boolean deliveryPossible = deliveryDistance <= 100;
-        if(deliveryPossible){
-        if (close) {
-            estimate += 1;
-            System.out.println("потребуется дней " + estimate);
-        } else {
-            if (middle) {
+        if (deliveryPossible) {
+            if (close) {
                 estimate += 1;
-                estimate +=1;
-                System.out.println("Потребуется дней " + estimate);}
+                System.out.println("потребуется дней " + estimate);
+            } else {
+                if (middle) {
+                    estimate += 2;
 
-                else  if (far)
-            estimate += 1;
-                estimate +=1;
-                estimate +=1;
-        System.out.println("потребуется дней " + estimate);}}
-        else
-            {
-                System.out.println("Доставка в ваш регион не осуществляется");
-    }
+                    System.out.println("Потребуется дней " + estimate);
+                } else if (far)
+                    estimate += 3;
+                System.out.println("потребуется дней " + estimate);
+            }
+        } else {
+            System.out.println("Доставка в ваш регион не осуществляется");
+        }
         System.out.println(" ");
         System.out.println("Задача 5");
-byte monthNumber = 12;
-byte season = 0;
-boolean spring=(monthNumber>=3)&&(monthNumber<=5);
-boolean summer=(monthNumber>=6)&&(monthNumber<=8);
-boolean fall=(monthNumber>=9)&&(monthNumber<=11);
-boolean winter=(monthNumber<=2||monthNumber==12);
-boolean impossible=monthNumber>12;
-if (winter) {season=4;}
-else {
-    if (spring) {season = 1;}
-    else if (summer) {season = 2;}
-    else if (fall) {season = 3;}
-    else if(impossible) {
-        System.out.println("нет такого месяца");
+        byte monthNumber = 12;
+        boolean spring = (monthNumber >= 3) && (monthNumber <= 5);
+        boolean summer = (monthNumber >= 6) && (monthNumber <= 8);
+        boolean fall = (monthNumber >= 9) && (monthNumber <= 11);
+        boolean winter = (monthNumber <= 2 || monthNumber == 12);
+        boolean impossible = monthNumber > 12;
+        if (!impossible) {
+            switch (monthNumber) {
+                case 1:
+                    System.out.println("Январь - это Зима!");
+                    break;
+                case 2:
+                    System.out.println("Февраль - это Зима!");
+                    break;
+                case 3:
+                    System.out.println("Март - это Весна!");
+                    break;
+                case 4:
+                    System.out.println("Апрель - это Весна!");
+                    break;
+                case 5:
+                    System.out.println("Май - это Весна!");
+                    break;
+                case 6:
+                    System.out.println("Июнь - это Лето!");
+                    break;
+                case 7:
+                    System.out.println("Июль - это Лето!");
+                    break;
+                case 8:
+                    System.out.println("Август - это Лето!");
+                    break;
+                case 9:
+                    System.out.println("Сентябрь - это Осень!");
+                    break;
+                case 10:
+                    System.out.println("Октябрь - это Осень!");
+                    break;
+                case 11:
+                    System.out.println("Ноябрь - это Осень!");
+                    break;
+                case 12:
+                    System.out.println("Декабрь - это Зима!");
+                    break;
+            }
+        } else {
+            System.out.println("Такого Месяца не существует");
+        }
+
     }
 }
-switch (season) {
-    case 1:
-        System.out.println("Весна!");
-        break;
-    case 2:
-        System.out.println("Лето!");
-        break;
-    case 3:
-        System.out.println("Осень!");
-        break;
-    case 4:
-        System.out.println("Зима!");
-        break;
-    }
-
-
-        }
-    }
 
